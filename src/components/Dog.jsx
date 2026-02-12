3fd059f4becd256c6464894ed82d37c1e68675cb
KIRRI THARUN <tharunprajitha2023@gmail.com>
2026-02-12 19:57:33 +0530

    Updated Dog Model

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,84 +1,54 @@
 import React from 'react'
 import * as THREE from "three"
+import { useEffect } from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
 import { BoxGeometry } from 'three'
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
-import { color, normalMap } from 'three/tsl'
+import { OrbitControls, useGLTF, useTexture, useAnimations } from '@react-three/drei'
+
 
 export const Dog = () => {
-  const model = useGLTF("/dog.drc.glb");
+  const model = useGLTF("/dog.drc.glb"); // to load the model
   useThree(({ camera, scene, gl }) => {
     camera.position.z = 0.7;
+    gl.toneMapping = THREE.ReinhardToneMapping  // fixing the renderer for good colors
+    gl.outputColorSpace = THREE.SRGBColorSpace // fixing the renderer for good colors
+  })
+  const {actions}=useAnimations(model.animations,model.scene);
+  useEffect(()=>{
+    actions["Take 001"].play()
+  },[actions])
+
+  const [normalMap, sampleMapCap,branchNormalMap,branchMap] = (useTexture(["/dog_normals.jpg", "/2.jpg","/branches_normals.jpg","/branches_diffuse.jpg"])).map(texture => {
+    texture.flipY = false
+    texture.colorSpace = THREE.SRGBColorSpace
+    return texture
   })
 
-  const  texture  = useTexture({
-    normalMap: "/dog_normals.jpg"
+  const dogMaterial = new THREE.MeshMatcapMaterial({  // adding the normal map for each objext related to Dog
+    normalMap: normalMap,
+    matcap: sampleMapCap // appling the color that reflect according to the light
+  })
+  
+  const branchMaterial= new THREE.MeshMatcapMaterial({
+    normalMap:branchNormalMap,
+    map:branchMap
   })
 
-  texture.normalMap.flipY=false;
-
-  model.scene.traverse((child) => {
+  model.scene.traverse((child) => {  // to traverse each object of the model 
     if (child.name.includes("DOG")) {
-      child.material = new THREE.MeshStandardMaterial({
-        normalMap: texture.normalMap
-      })
+      child.material = dogMaterial
+    }
+    else{
+      child.material=branchMaterial
     }
   })
-
   return (
     <>
-      {/* For testing Cube Purpose*/}
-      {/* <mesh>
-        <meshBasicMaterial color={0x00FF00} />
-        <boxGeometry args={[1, 1, 1]} />
-      </mesh> 
-      */}
-
       <primitive object={model.scene} position={[0.25, -0.5, 0]} rotation={[0, Math.PI / 4, 0]} />
       <ambientLight intensity={0.5} />
 
       <directionalLight color={0xFFFFFF} intensity={10} />
-      <OrbitControls />
+      {/* <OrbitControls /> */}
     </>
   )
 }
-
-// import React, { useEffect } from "react";
-// import * as THREE from "three";
-// import { useThree } from "@react-three/fiber";
-// import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
-
-// export const Dog = () => {
-//   const model = useGLTF("/dog.drc.glb");
-//   const { normalMap } = useTexture({
-//     normalMap: "/dog_normals.jpg"
-//   });
-
-//   const { camera } = useThree();
-
-//   useEffect(() => {
-//     camera.position.set(0, 0, 0.7);
-
-//     model.scene.traverse((child) => {
-//       if (child.isMesh && child.name.includes("DOG")) {
-//         child.material = new THREE.MeshStandardMaterial({
-//           normalMap: normalMap
-//         });
-//       }
-//     });
-//   }, [model, normalMap]);
-
-//   return (
-//     <>
-//       <primitive
-//         object={model.scene}
-//         position={[0.25, -0.5, 0]}
-//         rotation={[0, Math.PI / 4, 0]}
-//       />
-
-//       <directionalLight intensity={2} />
-
-//       <OrbitControls />
-//     </>
-//   );
-// };
